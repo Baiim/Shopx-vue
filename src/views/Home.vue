f7bca537dca083a2fc339e644d74c900c662b5b2
Baiim <sektia7@gmail.com>
2021-08-13 23:06:11 +0700

    First Blood

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <!-- Header -->
+    <HeaderShop />
+    <HeroShop />
+    <BannerShop />
+    <InstaShop />
+    <PartnerShop />
+    <FooterShop />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShop from "@/components/HeaderShop.vue"
+import HeroShop from "@/components/HeroShop.vue"
+import BannerShop from "@/components/BannerShop.vue"
+import InstaShop from "@/components/InstaShop.vue"
+import PartnerShop from "@/components/PartnerShop.vue"
+import FooterShop from "@/components/FooterShop.vue"
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShop,
+    HeroShop,
+    BannerShop,
+    InstaShop,
+    PartnerShop,
+    FooterShop,
+  },
+}
+</script>
